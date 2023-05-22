1be2d3c25636ac62b1d1defa583dad0d75171766
LVtomatoj <992203755@qq.com>
2023-05-22 19:07:22 +0800

    增加admin和user路由模块

--- src/fastapi-project/main.py
+++ src/fastapi-project/main.py
@@ -1,13 +1,16 @@
 from fastapi import Depends,FastAPI
-from .routers import users,guest,base
-from .dependencies import get_current_user
+from .routers import users,guest,base,admin,live
+from .dependencies import check_token_user,check_token_ok
+from .models import User,OutUser
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(guest.router)
 app.include_router(base.router)
-@app.get('/')
-async def root(current_user = Depends(get_current_user)):
-    return current_user
+app.include_router(admin.router)
+app.include_router(live.router)
 
+@app.get('/')
+async def root(user:User = Depends(check_token_user)):
+    return OutUser(**user.dict())
