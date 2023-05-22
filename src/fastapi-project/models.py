ae57b2c57b6f203b32c35bab19f3056f1f2c30c7
LVtomatoj <992203755@qq.com>
2023-05-22 19:07:44 +0800

    增加user和live相关model

--- src/fastapi-project/models.py
+++ src/fastapi-project/models.py
@@ -1,20 +1,24 @@
 from typing import Union
-from pydantic import BaseModel, conint, validator
+from pydantic import BaseModel, Field, conint, validator
+from bson import ObjectId
+from datetime import datetime
+from typing import Optional
 import re
+
 
 class Power(BaseModel):
     level: conint(ge=0, le=99)  # 这里使用coint限制level的值只能是int类型0-99
-    live_max_people_num: int
-    is_admin: conint(ge=0, le=1)  # 这里使用coint限制is_admin的值只能是int类型0或1
+    live_max_people_num: conint(ge=0, le=99)
 
-
-class User(BaseModel):
+class OutUser(BaseModel):
+    id:str
     username: str
-    password: str
     phone_number: str
     level: conint(ge=0, le=99)  # 这里使用coint限制level的值只能是int类型0-99
     livetime: conint(ge=0, le=9999)  # 这里使用coint限制level的值只能是int类型0-9999
-
+    is_admin:conint(ge=0,le=1) #是否admin 0-1
+    class Config:
+        arbitrary_types_allowed = True
     # 用户名长度校验
     @validator('username')
     def check_username_length(cls, v):
@@ -38,6 +42,11 @@ class User(BaseModel):
             raise ValueError('手机号格式错误')
         return v
 
+class User(OutUser):
+    password: str
+
+
+#token
 
 class Token(BaseModel):
     access_token: str
@@ -47,3 +56,28 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     username: str
     guest:int
+
+#live
+
+class InLive(BaseModel):
+    title:str
+    max_people:conint(ge=0, le=99)
+    times:conint(ge=0, le=99)
+    is_public:conint(ge=0,le=1)
+    password:str
+
+    # 标题长度校验
+    @validator('title')
+    def check_title_length(cls, v):
+        if len(v) < 3 or len(v) > 20:
+            raise ValueError('标题长度为3-20位')
+        return v
+    
+class Live(InLive):
+    id:Optional[str] = None
+    user_id:str
+    status:conint(ge=0,le=2)
+    endtime:datetime
+    times:Optional[int] = None
+
+
