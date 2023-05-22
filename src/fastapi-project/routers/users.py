8e2eeebc983b0fe0793565637e606d3433c467f4
LVtomatoj <992203755@qq.com>
2023-05-22 19:09:38 +0800

    demo

--- src/fastapi-project/routers/users.py
+++ src/fastapi-project/routers/users.py
@@ -1,3 +1,8 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter,Depends
+from ..dependencies import check_token_user
+from ..models import OutUser
 router = APIRouter(prefix='/user')
+
+@router.get('/info')
+async def get_user_info(user = Depends(check_token_user)):
+    return OutUser(**user.dict()).dict()
