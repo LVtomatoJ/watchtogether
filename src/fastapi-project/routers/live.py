82799fcdcc83635007f554335e6a4e842eab18d4
LVtomatoj <992203755@qq.com>
2023-05-23 01:56:00 +0800

    模块化 生成链接函数 添加websocket的demo函数（待补充

--- src/fastapi-project/routers/live.py
+++ src/fastapi-project/routers/live.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter,Depends,HTTPException,status
-from ..dependencies import check_token_user
-from ..models import OutUser,User,Power,InLive,Live
+from fastapi import APIRouter,Depends,HTTPException,status,WebSocket, WebSocketDisconnect
+from ..dependencies import check_token_ok, check_token_user
+from ..models import OutUser, TokenData,User,Power,InLive,Live
 from ..db.usemongo import get_power_by_level,add_live,update_user
 import datetime
 import hashlib
-from ..config import LIVE_KEY,LIVE_PUSH_DOMAIN
+from ..config import LIVE_KEY,LIVE_PUSH_DOMAIN,LIVE_PLAY_DOMAIN
+from fastapi.responses import HTMLResponse
 
 
-def make_push_url(endtime,live_id):
+
+
+def make_secret_time(end_time,live_id):
+    #生成txSecret,txTime
     md5 = hashlib.md5()
     end_time_stamp = end_time.timestamp()
     txTime = hex(int(end_time_stamp))[2:].upper()
@@ -15,8 +19,19 @@ def make_push_url(endtime,live_id):
     text = LIVE_KEY+'live_id'+ txTime
     md5.update(text.encode('utf-8'))
     txSecret = md5.hexdigest()
+    return txSecret,txTime
+
+def make_push_url(end_time,live_id):
+    #生成推流地址
+    txSecret,txTime = make_secret_time(end_time,live_id)
     push_url = r"rtmp://{}/live/{}?txSecret={}&txTime={}".format(LIVE_PUSH_DOMAIN,live_id,txSecret,txTime)
     return push_url
+
+def make_play_hls_url(end_time,live_id):
+    #生成HLS拉流地址
+    txSecret,txTime = make_secret_time(end_time,live_id)
+    play_url = r"{}/live/{}.m3u8?txSecret={}&txTime={}".format(LIVE_PUSH_DOMAIN,live_id,txSecret,txTime)
+    return play_url
 
 router = APIRouter(prefix='/live')
 
@@ -50,5 +65,22 @@ async def create_live(inlive:InLive,user:User = Depends(check_token_user)):
         #更新剩余时间错误
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="更新剩余时间错误")
     #获取推流地址
-    push_url = make_push_url(endtime=end_time,live_id=live_id)
-    return push_url
+    push_url = make_push_url(end_time=end_time,live_id=live_id)
+    #获取播放地址
+    play_url = make_play_hls_url(end_time=end_time,live_id=live_id)
+    return push_url,play_url
+
+
+@router.websocket("/ws/{live_id}")
+async def websocket_endpoint(websocket: WebSocket,live_id:str,token_data:TokenData=Depends(check_token_ok)):
+    try:
+        await websocket.accept()
+        #检查是否公开
+            #检查密码
+        #添加到redis在线用户和房间用户中
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        await websocket.close()
+        #将用户从在线用户和房间用户中移除
